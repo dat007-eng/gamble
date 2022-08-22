56b395e363475e6ccafab6e4e8dce85ad05b093e
Dat <s3819827@rmit.edu.vn>
2022-08-22 16:31:31 +0700

    Initial Commit

---
+++ gamble/gambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  gambleApp.swift
+//  gamble
+//
+//  Created by dat on 22/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct gambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
