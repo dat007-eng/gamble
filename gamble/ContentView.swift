ec3fc1d0e3d322e90e9e19bafbc2e6cac10c3df3
Dat <s3819827@rmit.edu.vn>
2022-08-25 14:23:41 +0700

    sample text of dice

--- gamble/ContentView.swift
+++ gamble/ContentView.swift
@@ -1,18 +1,102 @@
 //
 //  ContentView.swift
-//  gamble
+//  Shared
 //
-//  Created by dat on 22/08/2022.
+//  Created by Sam Rodriguez on 7/27/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "dice_1"
+    @State private var cpuCard = "back"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+        
+            Color(red: 0.00, green: 0.01, blue: 0.54).ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("casino_logo")
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    // Generate random number between 2-14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    // Update cards
+                    
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    
+                    //Update score
+                    
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    }
+                    else if playerRand < cpuRand {
+                        cpuScore += 1
+                    }
+                    
+                    
+                    
+                }, label: {
+                    Image("pressbutton")
+                })
+                
+                
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                        
+                    }
+                    Spacer()
+                }
+                Spacer()
+                
+            }
+        }
+            
+            
+        }
+        
+        
+        
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
